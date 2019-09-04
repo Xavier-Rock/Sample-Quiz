6f9a8f3dce5d83060aabe60f87efdeff52d5dc98
Francis <fra8593@gmail.com>
2019-09-04 18:52:03 +0530

    Sample Quiz app Basic setup

---
+++ SampleQuiz2/app/src/main/java/com/example/samplequiz/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.samplequiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        openQuizMainFragment(this);
+    }
+
+    private void openQuizMainFragment(MainActivity activity) {
+        androidx.fragment.app.FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        QuizFragment quizFragment = new QuizFragment();
+        fragmentTransaction.replace(R.id.fragment_container, quizFragment, "Quiz Fragment");
+        fragmentTransaction.addToBackStack(quizFragment.getClass().getSimpleName());
+        fragmentTransaction.commitAllowingStateLoss();
+        fragmentTransaction.setCustomAnimations(0, 0, 0, 0);
+    }
+}
